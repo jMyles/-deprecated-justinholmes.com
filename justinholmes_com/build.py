abd35ef4683bf983020d7771a189c0495b5f192b
jMyles <justin@justinholmes.com>
2023-01-04 19:12:18 -0500

    Already deployed POAP stuff.

--- justinholmes_com/build.py
+++ justinholmes_com/build.py
@@ -29,7 +29,9 @@ build_meta = {'data_checksum': hashes['data'],
 page_builder = PageBuilder(build_meta, force_rebuild=False)
 
 page = page_builder.build_page("index", template_name="shared/brief.html", root=True, active_context={'meta_description':"Justin Holmes music, blockchain bluegrass, video game music"})
+
 page = page_builder.build_page("music", root=True, compact=True)
+page = page_builder.build_page("music/ursa-minor", root=True, compact=True)
 
 page = page_builder.build_page("these-days", root=True, compact=True, template_name="shared/detail-brightback.html")
 page = page_builder.build_page("before", template_name="shared/time-series.html", root=True, compact=True)
@@ -37,6 +39,6 @@ page = page_builder.build_page("talks", template_name="shared/time-series.html",
 page = page_builder.build_page("tour", template_name="shared/time-series.html", root=True, compact=True)
 
 
-page = page_builder.build_page("covid19/best-argument-against-herd-immunity")
+# page = page_builder.build_page("covid19/best-argument-against-herd-immunity")
 
 
---
+++ public/apps/shared/js/checkPoap.js
@@ -0,0 +1,58 @@
+// import * as poapProxyABI from '../abi/poapABI.js';
+// import * as Web3 from './web3.min.js';
+let web3 = new window.Web3(window.ethereum);
+let POAPContract = new web3.eth.Contract(poapProxyABI, "0x22C1f6050E56d2876009903609a2cC3fEf83B415");
+export async function checkPoap(eventID) {
+    const account = web3.eth.accounts;
+    let walletAddress = account.givenProvider.selectedAddress;
+    console.log(`Wallet: ${walletAddress}`);
+    let POAPCount = await POAPContract.methods.balanceOf(walletAddress).call();
+    console.log("Total POAPS:", { POAPCount });
+    for (let poapIndex = 0; poapIndex < POAPCount; poapIndex++) {
+        let POAPToken = await POAPContract.methods.tokenDetailsOfOwnerByIndex(walletAddress, poapIndex).call();
+        console.log("event ID:");
+        console.log(POAPToken.eventId);
+        if (POAPToken.eventId == "95303") {
+            console.log("In Bogota!");
+            // bogota = true
+            // album_acess = true
+            //
+            //
+            // bogotaArt.classList.add('saturated')
+            // bogotaArt.classList.remove('unsaturated');
+            // bogotaArt.classList.remove('pulsing-red');
+        }
+    }
+}
+async function connect() {
+    if (window.ethereum) {
+        console.log("Getting wallet info.  Hey RJ.");
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        console.log("Changing Network");
+        let networkChange = window.ethereum.request({
+            method: "wallet_addEthereumChain",
+            params: [{
+                    chainId: "0x64",
+                    rpcUrls: ["https://rpc.gnosischain.com/"],
+                    chainName: "Gnosis",
+                    nativeCurrency: {
+                        name: "xDAI",
+                        symbol: "xDAI",
+                        decimals: 18
+                    },
+                    blockExplorerUrls: ["https://gnosisscan.io"]
+                }]
+        });
+        await networkChange;
+        console.log("Network change result:");
+        console.log(networkChange);
+        //////////////////
+        let bogota = false;
+        let album_acess = false;
+        let bogotaArt = document.getElementById("devconPOAP");
+        bogotaArt.classList.add('pulsing-red');
+    }
+    else {
+        console.log("No wallet");
+    }
+}
